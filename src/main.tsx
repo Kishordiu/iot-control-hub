cd85d9d31bab70fe1aebfc1347e43020fbb485f9
Kishordiu <kishorvishwa2525@gmail.com>
2026-02-28 22:18:21 +0530

    Recovered stable version

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "next-themes";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider
-    attribute="class"
-    defaultTheme="dark"
-    enableSystem={false}
-  >
-    <App />
-  </ThemeProvider>
+  <App />
 );
